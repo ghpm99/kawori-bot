531ecd3cf9091dd071272e3cca7d90abc57f7e2b
ghpm99 <ghpm99@hotmail.com>
2022-05-29 00:59:06 -0300

    Deleta message repository

--- src/main/java/com/kawori/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kawori.repository;
-
-import com.kawori.model.MessageDiscord;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<MessageDiscord, Long>{
-
-}
